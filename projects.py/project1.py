984e29854cc1b80cd3b448ef1d89a936ee5e7706
Arnav2277 <arnavsharmanz127@gmail.com>
2026-04-13 15:13:11 +1200

    flow chart

---
+++ projects.py/project1.py
@@ -0,0 +1,11 @@
+# Print a Sentence
+print("Hello Everyone")
+
+# Print a Number
+print(4)
+
+# Print both simultaneously
+print("the number being printed is", 4)
+
+# Print an empty line before/after a sentence
+print("\nI am Penguin")
