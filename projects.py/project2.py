a4cca40b8b4200883dc4207d390a1d767cab8cca
Arnav2277 <arnavsharmanz127@gmail.com>
2026-04-15 19:38:36 +1200

    Number swapping

---
+++ projects.py/project2.py
@@ -0,0 +1,12 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+print("Before swapping:")
+print("First number =", a)
+print("Second number =", b)
+print("Third number =", c)
+a, b, c = c, a, b
+print("After swapping:")
+print("First number =", a)
+print("Second number =", b)
+print("Third number =", c)
