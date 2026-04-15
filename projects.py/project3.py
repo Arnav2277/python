52aaedd472d98b4ff90d2de6883cad576539990a
Arnav2277 <arnavsharmanz127@gmail.com>
2026-04-15 19:42:42 +1200

    Exam Eligibility

---
+++ projects.py/project3.py
@@ -0,0 +1,6 @@
+attendance = int(input("Enter attendance percentage: "))
+
+if attendance >= 75:
+    print("You are eligible to write the exam.")
+else:
+    print("You are not eligible to write the exam.")
